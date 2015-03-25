3b66f2227528da41c93472cb7809c7e56924be81
Fletcher Nichol <fnichol@nichol.ca>
2015-03-25 09:18:48 -0600

    Add header to bin/chefdk-update-app.rb.

--- bin/chefdk-update-app.rb
+++ bin/chefdk-update-app.rb
@@ -1,4 +1,21 @@
 #!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+#
+# Author:: Fletcher Nichol (<fnichol@nichol.ca>)
+#
+# Copyright (C) 2015 Fletcher Nichol
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 require "pathname"
 require "optparse"
@@ -128,7 +145,6 @@ class Updater
 end
 
 class CLI
-
   def self.options
     new.options
   end
